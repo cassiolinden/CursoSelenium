8e15c017bd83aaad31cb5d8bc0c105432478520c
Cassio Linden Albert <cassio@cr.local>
2019-06-25 14:03:29 -0300

    Conclusão da aula "63. Espera explícita"

--- src/main/java/TesteSincronismo.java
+++ src/main/java/TesteSincronismo.java
@@ -3,8 +3,11 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TesteSincronismo {
 	private WebDriver driver;
@@ -30,11 +33,20 @@ public class TesteSincronismo {
 			Thread.sleep(5000);
 			dsl.escrever("novoCampo", "Deu certo?");
 		}
+
 		@Test
 		public void deveUtilizarEsperaImplicita() throws InterruptedException {
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 			dsl.clicarBotao("buttonDelay");
 			dsl.escrever("novoCampo", "Deu certo?");
 			driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+		}
+		
+		@Test
+		public void deveUtilizarEsperaExplicita() throws InterruptedException {
+			dsl.clicarBotao("buttonDelay");
+			WebDriverWait wait = new WebDriverWait(driver, 30);
+			wait.until(ExpectedConditions.presenceOfElementLocated(By.id("novoCampo")));
+			dsl.escrever("novoCampo", "Deu certo?");
 		}
 }
